ebdb597e4bf206723f3df593377f32d5de77e934
saulius <sjsaulius@gmail.com>
2022-09-24 11:34:40 +0300

    render socials

---
+++ js/data/socialsData.js
@@ -0,0 +1,33 @@
+const socialsData = [
+  {
+    href: "#",
+    icon: "facebook",
+  },
+  {
+    href: "#",
+    icon: "twitter",
+  },
+  {
+    href: "#",
+    icon: "dribbble",
+  },
+  {
+    href: "#",
+    icon: "tumblr",
+  },
+  {
+    href: "#",
+    icon: "instagram",
+  },
+
+  {
+    href: "#",
+    icon: "linkedin",
+  },
+  {
+    href: "#",
+    icon: "rss",
+  },
+];
+
+export { socialsData };
--- js/home.js
+++ js/home.js
@@ -1,7 +1,10 @@
 // import
 
 import { Clock } from "./components/clock/Clock.js";
+import { socials } from "./components/socials/socials.js";
+import { socialsData } from "./data/socialsData.js";
 
 // EXECUTION
 
 Clock("#clock");
+socials("footer .socials", socialsData);
